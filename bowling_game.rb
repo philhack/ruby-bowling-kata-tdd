5015c9c7053a3e310b76ef2c5983c5d93d5142f6
Phil Hack <phil@philhack.com>
2018-02-27 19:07:08 +0000

    refactor -> extract method

--- bowling_game.rb
+++ bowling_game.rb
@@ -9,7 +9,7 @@ class BowlingGame
     first_in_frame = 0
 
     while frame < 10
-      if @rolls[first_in_frame] + @rolls[first_in_frame + 1] == 10
+      if spare?(first_in_frame)
         score += 10 + @rolls[first_in_frame + 2]
       else
         score += @rolls[first_in_frame] + @rolls[first_in_frame + 1]
@@ -20,5 +20,10 @@ class BowlingGame
     end
 
     score
+  end
+
+  private
+  def spare?(first_in_frame)
+    @rolls[first_in_frame] + @rolls[first_in_frame + 1] == 10
   end
 end
